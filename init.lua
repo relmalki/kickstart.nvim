e6a2d92eb3c157ed4317c1e9adf73da92b090657
reditall <reditall>
2024-07-17 10:39:29 +0200

    add changes

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -278,21 +278,6 @@ require('lazy').setup({
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
@@ -878,6 +863,23 @@ require('lazy').setup({
     end,
   },
   {
+    'rcarriga/nvim-notify',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {},
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/plenary.nvim',
+    },
+
+    config = function()
+      require('telescope').load_extension 'lazygit'
+    end,
+  },
+  {
     'epwalsh/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = true,
@@ -898,13 +900,17 @@ require('lazy').setup({
     opts = {
       workspaces = {
         {
-          name = 'personal',
-          path = '~/repos/mee/lifeeasy/funny/personal',
-        },
-        {
           name = 'funny',
           path = '~/repos/mee/lifeeasy/funny/funny',
         },
+        {
+          name = 'personal',
+          path = '~/repos/mee/lifeeasy/funny/personal',
+        },
+      },
+
+      daily_notes = {
+        folder = 'dailies',
       },
 
       -- see below for full list of options 👇
